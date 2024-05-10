5c7cb3ac4737d36ca82ddf103affa57f5b093bb6
Ulimhuka Agbama <ulimhukaakem@gmail.com>
2024-05-10 16:30:12 +0100

    Fixed Null Errors

--- lib/src/primary_button.dart
+++ lib/src/primary_button.dart
@@ -15,14 +15,14 @@ class ChowButton extends StatelessWidget {
       this.disabled,
       this.iconWidget,
       this.borderRadius = 8});
-  final double? width;
-  final double? height;
-  final String? title;
-  final bool? loading;
-  final bool? icon;
+  final double width;
+  final double height;
+  final String title;
+  final bool loading;
+  final bool icon;
   final Widget? iconWidget;
-  final double? fontSize;
-  final VoidCallback? onTap;
+  final double fontSize;
+  final VoidCallback onTap;
   final double borderRadius;
   final Color? color;
   final TextStyle? textStyle;
@@ -41,10 +41,10 @@ class ChowButton extends StatelessWidget {
           )),
           textStyle: MaterialStateProperty.all<TextStyle>(textStyle ??
               TextStyle(fontSize: fontSize, fontWeight: FontWeight.w500)),
-          maximumSize: MaterialStateProperty.all<Size>(Size.fromWidth(width!)),
-          fixedSize: MaterialStateProperty.all<Size>(Size.fromHeight(height!))),
+          maximumSize: MaterialStateProperty.all<Size>(Size.fromWidth(width)),
+          fixedSize: MaterialStateProperty.all<Size>(Size.fromHeight(height))),
       child: Center(
-        child: loading!
+        child: loading
             ? const SizedBox(
                 height: 22,
                 width: 22,
@@ -54,12 +54,12 @@ class ChowButton extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Visibility(visible: icon! ? true : false, child: iconWidget!),
+                  Visibility(visible: icon ? true : false, child: iconWidget ?? const SizedBox()),
                   const SizedBox(
                     width: 10
                   ),
                   Text(
-                    title!,
+                    title,
                     textAlign: TextAlign.center,
                     style: textStyle ??
                         TextStyle(
