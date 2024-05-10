b61f1382c50dcc13e51fa3f4926d08ef6eb26555
Ulimhuka Agbama <ulimhukaakem@gmail.com>
2024-05-10 16:53:53 +0100

    Working Version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 1.0.3
+## 1.0.6
 
 * Basic primary Button Functions
--- lib/src/primary_button.dart
+++ lib/src/primary_button.dart
@@ -22,7 +22,7 @@ class ChowButton extends StatelessWidget {
   final bool icon;
   final Widget? iconWidget;
   final double fontSize;
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
   final double borderRadius;
   final Color? color;
   final TextStyle? textStyle;
@@ -31,43 +31,36 @@ class ChowButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FilledButton(
-      onPressed: onTap,
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(
-              color ?? Theme.of(context).colorScheme.primary),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(borderRadius)
-          )),
-          textStyle: MaterialStateProperty.all<TextStyle>(textStyle ??
-              TextStyle(fontSize: fontSize, fontWeight: FontWeight.w500)),
-          maximumSize: MaterialStateProperty.all<Size>(Size.fromWidth(width)),
-          fixedSize: MaterialStateProperty.all<Size>(Size.fromHeight(height))),
-      child: Center(
-        child: loading
-            ? const SizedBox(
-                height: 22,
-                width: 22,
-                child: CircularProgressIndicator()
-              )
-            : Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Visibility(visible: icon ? true : false, child: iconWidget ?? const SizedBox()),
-                  const SizedBox(
-                    width: 10
-                  ),
-                  Text(
-                    title,
-                    textAlign: TextAlign.center,
-                    style: textStyle ??
-                        TextStyle(
-                            fontSize: fontSize, fontWeight: FontWeight.w500)
-                  )
-                ]
-              )
-      )
-    );
+        onPressed: onTap,
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+                color ?? Theme.of(context).colorScheme.primary),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(borderRadius))),
+            textStyle: MaterialStateProperty.all<TextStyle>(textStyle ??
+                TextStyle(fontSize: fontSize, fontWeight: FontWeight.w500)),
+            maximumSize: MaterialStateProperty.all<Size>(Size.fromWidth(width)),
+            fixedSize:
+                MaterialStateProperty.all<Size>(Size.fromHeight(height))),
+        child: Center(
+            child: loading
+                ? const SizedBox(
+                    height: 22, width: 22, child: CircularProgressIndicator())
+                : Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                        Visibility(
+                            visible: icon,
+                            child: iconWidget ?? const SizedBox()),
+                        SizedBox(width: icon ? 10 : 0),
+                        Text(title,
+                            textAlign: TextAlign.center,
+                            style: textStyle ??
+                                TextStyle(
+                                    fontSize: fontSize,
+                                    fontWeight: FontWeight.w500))
+                      ])));
   }
 }
