d511d3c88011386a39c36214da592f9600e762ee
Ulimhuka Agbama <ulimhukaakem@gmail.com>
2024-05-10 15:40:21 +0100

    Example Parameters Added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:chow_button/chow_button.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,13 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -25,7 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -33,26 +31,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
       appBar: AppBar(
-    
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    
         title: Text(widget.title),
       ),
       body: Center(
-   child: 
-       ChowButton(
-          onTap: (){},
-          title: 'Button',
-          
-          ),
+        child: ChowButton(
+          onTap: () {
+            print('Tapped');
+          },
+          title: 'Proceed',
+          height: 50,
+          loading: false,
+          disabled: false,
+          textStyle: const TextStyle(fontSize: 16),
+          color: Colors.orange,
+          width: double.infinity,
+          icon: true,
+          iconWidget: const Icon(Icons.pause),
+        ),
       ),
-      
     );
   }
 }
